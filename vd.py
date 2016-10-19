7652ffda8282af210dbdd334f07a1df68c2e3214
Simon Dahlberg <lin503_simon@mumin.ling.su.se>
2016-10-19 14:09:26 +0200

    naestan klar

---
+++ vd.py
@@ -0,0 +1,461 @@
+
+# -*- coding: utf-8 -*-
+
+verblista = {"acceptera": ("accepterar", "accepterade", "accepterat")}
+verblista["anlända"] = ("anländer", "anlände", "anlänt")
+verblista["använda"] = ("använder", "anlände", "använt")
+verblista["be"] = ("ber", "bad", "bett")
+verblista["begripa"] = ("begriper", "begrep", "begripit")
+verblista["bero"] = ("beror", "berodde", "berott")
+verblista["bestämma"] = ("bestämmer", "bestämde", "bestämt")
+verblista["betala"] = ("betalar", "betalade", "betalat")
+verblista["beträffa"] = ("beträffar", "beträffade", "beträffat")
+verblista["brinna"] = ("brinner", "brann", "brunnit")
+verblista["bränna"] = ("bränner", "brände", "bränt")
+verblista["bry"] = ("bryr", "brydde", "brytt")
+verblista["byta"] = ("byter", "bytte", "bytt")
+verblista["böja"] = ("böjer", "böjde", "böjt")
+verblista["böra"] = ("bör", "borde", "bort")
+verblista["diskutera"] = ("diskuterar", "diskuterade", "diskuterat")
+verblista["dricka"] = ("dricker", "drack", "druckit")
+verblista["dölja"] = ("döljer", "döljde", "döljt")
+verblista["erfara"] = ("erfar", "erfor", "erfarit")
+verblista["falla"] = ("faller", "föll", "fallit")
+verblista["flyga"] = ("flyger", "flög", "flugit")
+verblista["fortsätta"] = ("fortsätter", "fortsatte", "fortsatt")
+verblista["fräta"] = ("fräter", "frätte", "frätt")
+verblista["följa"] = ("följer", "följde", "följt")
+verblista["förbereda"] = ("förbereder", "förberedde", "förberett")
+verblista["förstå"] = ("förstår", "förstod", "förstått")
+verblista["förändra"] = ("förändrar", "förändrade", "förändrat")
+verblista["gå"] = ("går", "gick", "gått")
+verblista["göra"] = ("gör", "gjorde", "gjort")
+verblista["ha"] = ("har", "hade", "haft")
+verblista["hinna"] = ("hinner", "hann", "hunnit")
+verblista["hoppas"] = ("hoppas", "hoppades", "hoppats")
+verblista["hämta"] = ("hämtar", "hämtade", "hämtat")
+verblista["jobba"] = ("jobbar", "jobbade", "jobbat")
+verblista["jämföra"] = ("jämför", "jämförde", "jämfört")
+verblista["klä"] = ("klär", "klädde", "klätt")
+verblista["kläda"] = ("kläder", "klädde", "klätt")
+verblista["knyta"] = ("knyter", "knöt", "knutit")
+verblista["komma"] = ("kommer", "kom", "kommit")
+verblista["krypa"] = ("kryper", "kröp", "krupit")
+verblista["kunna"] = ("kan", "kunde", "kunnat")
+verblista["känna"] = ("känner", "kände", "känt")
+verblista["köra"] = ("kör", "körde", "kört")
+verblista["landa"] = ("landar", "landade", "landat")
+verblista["le"] = ("ler", "log", "lett")
+verblista["leka"] = ("leker", "lekte", "lekt")
+verblista["leva"] = ("lever", "levde", "levt")
+verblista["lida"] = ("lider", "led", "lidit")
+verblista["ligga"] = ("ligger", "låg", "legat")
+verblista["låta"] = ("låter", "lät", "låtit")
+verblista["lägga"] = ("lägger", "lade", "lagt")
+verblista["läsa"] = ("läser", "läste", "läst")
+verblista["lösa"] = ("löser", "löste", "löst")
+verblista["markerade"] = ("markerar", "markerade", "markerat")
+verblista["må"] = ("mår", "mådde", "mått")
+verblista["märka"] = ("märker", "märkte", "märkt")
+verblista["möta"] = ("möter", "mötte", "mött")
+verblista["njuta"] = ("njuter", "njöt", "njutit")
+verblista["peka"] = ("pekar", "pekade", "pekat")
+verblista["plugga"] = ("pluggar", "pluggade", "pluggat")
+verblista["påverka"] = ("påverkar", "påverkade", "påverkat")
+verblista["reflektera"] = ("reflekterar", "reflekterade", "reflekterat")
+verblista["repetera"] = ("repeterar", "repeterade", "repeterat")
+verblista["ringa"] = ("ringer", "ringde", "ringt")
+verblista["röra"] = ("rör", "rörde", "rört")
+verblista["se"] = ("ser", "såg", "sett")
+verblista["sitta"] = ("sitter", "satt", "suttit")
+verblista["sjunga"] = ("sjunger", "sjöng", "sjungit")
+verblista["skina"] = ("skiner", "sken", "skinit")
+verblista["skjuta"] = ("skjuter", "sköt", "skjutit")
+verblista["skola"] = ("ska", "skulle", "skolat")
+verblista["skratta"] = ("skrattar", "skrattade", "skrattat")
+verblista["skriva"] = ("skriver", "skrev", "skrivit")
+verblista["spela"] = ("spelar", "spelade", "spelat")
+verblista["sluta"] = ("slutar", "slutade", "slutat")
+verblista["släcka"] = ("släcker", "släckte", "släckt")
+verblista["slösa"] = ("slösar", "slösade", "slösat")
+verblista["smila"] = ("smilar", "smilade", "smilat")
+verblista["sova"] = ("sover", "sov", "sovit")
+verblista["spinna"] = ("spinner", "spann", "spunnit")
+verblista["sprida"] = ("sprider", "spred", "spritt")
+verblista["springa"] = ("springer", "sprang", "sprungit")
+verblista["stanna"] = ("stannar", "stannade", "stannat")
+verblista["stiga"] = ("stiger", "steg", "stigit")
+verblista["stå"] = ("står", "stod", "stått")
+verblista["socka"] = ("suckar", "suckade", "suckat")
+verblista["supa"] = ("super", "söp", "supit")
+verblista["så"] = ("sår", "sådde", "sått")
+verblista["säga"] = ("säger", "sade", "sagt")
+verblista["sätta"] = ("sätter", "satte", "satt")
+verblista["sälja"] = ("säljer", "sålde", "sålt")
+verblista["söka"] = ("söker", "sökte", "sökt")
+verblista["symbolisera"] = ("symboliserade", "symboliserade", "symboliserat")
+verblista["ta"] = ("tar", "tog", "tagit")
+verblista["tillåta"] = ("tillåter", "tillät", "tillåtit")
+verblista["titta"] = ("tittar", "tittade", "tittat")
+verblista["triva"] = ("ttiver", "trivde", "trivt")
+verblista["träffa"] = ("träffar", "träffade", "träffat")
+verblista["tvinga"] = ("tvingar", "tvingade", "tvingat")
+verblista["tycka"] = ("tycker", "tyckte", "tyckt")
+verblista["täcka"] = ("täcker", "täckte", "täckt")
+verblista["tänka"] = ("tänker", "tänkte", "tänkt")
+verblista["vända"] = ("vänder", "vände", "vänt")
+verblista["umgå"] = ("umgår", "umgick", "umgått")
+verblista["uppskatta"] = ("uppskattar", "uppskattade", "uppskattat")
+verblista["upptäcka"] = ("upptäcker", "upptäckte", "upptäckt")
+verblista["utnyttja"] = ("utnyttjar", "utnyttjade", "utnyttjat")
+verblista["veta"] = ("vet", "visste", "vetat")
+verblista["vara"] = ("är", "var", "varit")
+verblista["vinka"] = ("vinkar", "vinkade", "vinkat")
+verblista["visa"] = ("visar", "visade", "visat")
+verblista["välja"] = ("väljer", "valde", "valt")
+verblista["åka"] = ("åker", "åkte", "åkt")
+verblista["äga"] = ("äger", "ägde", "ägt")
+verblista["älska"] = ("älskar", "älskade", "älskat")
+verblista["äta"] = ("äter", "åt", "ätit")
+verblista["önska"] = ("önskar", "önskade", "önskat")
+verblista["öppna"] = ("öppnar", "öppnade", "öppnat")
+verblista["översätta"] = ("översätter", "översatte", "översatt")
+
+
+class VerbDetektiv:
+    def __init__(self, verblista):
+        self.verbl = verblista
+
+    def raetta(self, felboejt_verb):
+       
+        raettad_form = self.slaa_upp(felboejt_verb)
+
+        if raettad_form != None:
+            print ("Inget fel!")
+            felboejt_verb.raettning = raettad_form
+            return felboejt_verb.raettning
+        else:
+            felboejt_verb.kraanglighet += 1
+            raettad_form = self.aendelseanalys(felboejt_verb)
+
+        if raettad_form == None:
+            felboejt_verb.kraanglighet += 1
+            raettad_form = self.vokalfel(felboejt_verb) # kollar vokalfel
+
+        if raettad_form == None:
+            felboejt_verb.kraanglighet += 1
+            raettad_form = self.dblkonsfel(felboejt_verb) # kollar dubbelkonsonantfel
+
+        if raettad_form != None:
+            if felboejt_verb.passiv == True:
+                if felboejt_verb.raettning[len(felboejt_verb.raettning) - 1:] == 'r':
+                    felboejt_verb.raettning = felboejt_verb.raettning[:len(felboejt_verb.raettning) - 1]
+                felboejt_verb.raettning = felboejt_verb.raettning + 's'
+            #print("felboejt_verb.kraanglighet: " + str(felboejt_verb.kraanglighet))
+            return felboejt_verb.raettning
+        else:
+            return "Inget hittat\n\n"
+
+    def slaa_upp(self, felboejt_verb):
+
+        print("inne i slå upp-metoden!")
+        for infinitiv in self.verbl.keys():
+            if felboejt_verb.raettning == infinitiv:
+                felboejt_verb.saetta_tempus(-1)
+                return infinitiv
+            elif felboejt_verb.kraanglighet == 0 or felboejt_verb.kraanglighet >= 2:
+                tempusindx = 0
+                for boejning in self.verbl[infinitiv]:
+                    if felboejt_verb.raettning == boejning:
+                        felboejt_verb.saetta_tempus(tempusindx)
+                        return boejning
+                    tempusindx += 1
+        return None
+
+    def aendelseanalys(self, felboejt_verb):
+        print("felboejt_verb.raettning: " + felboejt_verb.raettning)
+        ordets_laengd = len(felboejt_verb.raettning)
+        raettad_form = None
+        
+        if felboejt_verb.raettning[ordets_laengd - 2:] == "ar":
+            felboejt_verb.saetta_tempus(0)
+            raettad_form = self.grupp1(felboejt_verb)
+
+        elif felboejt_verb.raettning[ordets_laengd - 3:] == "ade":
+            felboejt_verb.saetta_tempus(1)
+            raettad_form = self.grupp1(felboejt_verb)
+
+        elif felboejt_verb.raettning[ordets_laengd - 2:] == "at":
+            felboejt_verb.saetta_tempus(2)
+            raettad_form = self.grupp1(felboejt_verb)
+
+        elif felboejt_verb.raettning[ordets_laengd - 3:] == "dde":
+            felboejt_verb.saetta_tempus(1)
+            raettad_form = self.grupp2(felboejt_verb)
+
+        elif felboejt_verb.raettning[ordets_laengd - 3:] == "tt":
+            felboejt_verb.saetta_tempus(2)
+            raettad_form = self.grupp2(felboejt_verb)
+
+        elif felboejt_verb.raettning[ordets_laengd - 3:] == "ede":
+            felboejt_verb.saetta_tempus(1)
+            raettad_form = self.slut_ede(felboejt_verb)
+
+        elif felboejt_verb.raettning[ordets_laengd - 2:] == "er":
+            felboejt_verb.saetta_tempus(0)
+            raettad_form = self.grupp3(felboejt_verb)
+
+        elif felboejt_verb.raettning[ordets_laengd - 2:] == "de" or felboejt_verb.raettning[ordets_laengd - 2:] == "te":
+            felboejt_verb.saetta_tempus(1)
+            raettad_form = self.grupp3(felboejt_verb)
+            if (raettad_form == None) and (felboejt_verb.raettning[ordets_laengd - 3:] == 'rde'):
+                raettad_form = self.slut_rde(felboejt_verb)
+
+        elif felboejt_verb.raettning[ordets_laengd - 2:] == "it":
+            felboejt_verb.saetta_tempus(2)
+            raettad_form = self.grupp3(felboejt_verb)
+
+        elif felboejt_verb.raettning[ordets_laengd-1:] == "t":
+            felboejt_verb.saetta_tempus(2)
+            raettad_form = self.grupp3(felboejt_verb)
+
+        elif felboejt_verb.raettning[ordets_laengd - 1] == 'e':
+            raettad_form = self.slut_e(felboejt_verb)
+        else:
+            print("ingen aendelse identifierad")
+
+        if raettad_form:
+            felboejt_verb.raettning = raettad_form
+        return raettad_form
+
+
+    def kolla_med_stavningsjustering(self, felboejt_verb):
+        if (self.slaa_upp(felboejt_verb)):
+            felboejt_verb.kraanglighet += 1
+            return felboejt_verb.raettning
+        elif (self.aendelseanalys(felboejt_verb)):
+            felboejt_verb.kraanglighet += 2
+            return felboejt_verb.raettning
+        else:
+            return None
+
+    def grupp1(self, felboejt_verb):
+        print ("grupp1-metoden")
+        aendelselaengd = 1
+        if felboejt_verb.tempus == 1:
+            aendelselaengd += 1
+
+        rot = felboejt_verb.raettning[:len(felboejt_verb.raettning) - aendelselaengd]
+
+        if rot in self.verbl.keys():
+            return (self.verbl[rot][felboejt_verb.tempus])
+        else:
+            return None
+
+    def grupp2(self, felboejt_verb):
+        print ("grupp2-metoden")
+        aendelselaengd = 2
+        if felboejt_verb.tempus == 1:
+            aendelselaengd += 1
+        rot = felboejt_verb.raettning[:len(felboejt_verb.raettning) - aendelselaengd]
+        if rot in self.verbl.keys():
+            return (self.verbl[rot][felboejt_verb.tempus])
+        else:
+            return None
+
+    def grupp3(self, felboejt_verb):
+        print ("grupp3-metoden")
+        aendelselaengd = 2
+        if felboejt_verb.tempus == 2:
+            aendelselaengd -= 1
+        rot = felboejt_verb.raettning[:len(felboejt_verb.raettning) - aendelselaengd] + 'a'  # vikte - vika
+
+        if rot not in self.verbl.keys():
+            if rot[:len(rot) - 1] in self.verbl.keys():  # gåde - gå
+                rot = rot[:len(rot) - 1]
+            else:  # brinde - brinna
+                konsonant_att_dubblas = felboejt_verb.raettning[
+                                        len(felboejt_verb.raettning) - (aendelselaengd+1):len(felboejt_verb.raettning) - aendelselaengd]
+                konsonantdubblad_rot = (felboejt_verb.raettning[:len(felboejt_verb.raettning) - aendelselaengd] + konsonant_att_dubblas + 'a')
+                if konsonantdubblad_rot in self.verbl.keys():
+                    rot = konsonantdubblad_rot
+                else:
+                    tonande_konsonanter = ('b', 'g', 'j', 'l', 'm', 'n', 'r', 'v')
+                    tonloesa_konsonanter = ('f', 'k', 'p', 's', 'x')
+                    if rot[len(rot)-2] in tonande_konsonanter:
+                        rot_med_tillaegg = rot[:len(rot)-1] + 'd' + 'a'
+                        if rot_med_tillaegg in self.verbl.keys():
+                            rot = rot_med_tillaegg
+                        else:
+                            return None
+                    elif rot[len(rot)-2] in tonloesa_konsonanter:
+                        rot_med_tillaegg = rot[:len(rot)-1] + 't' + 'a'
+                        if rot_med_tillaegg in self.verbl.keys():
+                            rot = rot_med_tillaegg
+                        else:
+                            return None
+                    else:                   
+                        return None
+        return (self.verbl[rot][felboejt_verb.tempus])
+
+    def slut_rde(self, felboejt_verb):
+        print ("rde-metoden")
+        uo = felboejt_verb.raettning
+        felboejt_verb.raettning = felboejt_verb.raettning[:len(felboejt_verb.raettning)-3] + felboejt_verb.raettning[len(felboejt_verb.raettning)-2:]
+        print(felboejt_verb.raettning)
+        #behöverde -> behövede
+        
+        if (self.kolla_med_stavningsjustering(felboejt_verb)):
+            return felboejt_verb.raettning
+        else:
+            felboejt_verb.raettning = uo
+            return None
+       
+    def slut_e(self, felboejt_verb):
+        uo = felboejt_verb.raettning
+
+        felboejt_verb.raettning += 'r'
+        print("i slut_e-metoden, är uo just nu: " + uo)
+        if (self.kolla_med_stavningsjustering(felboejt_verb)):
+            felboejt_verb.saetta_tempus(0)
+            return felboejt_verb.raettning
+        felboejt_verb.tempus = -1
+        felboejt_verb.raettning = uo[:len(uo)-1]
+        if (self.kolla_med_stavningsjustering(felboejt_verb)):
+            return felboejt_verb.raettning
+        felboejt_verb.raettning = uo[:len(uo) - 1] + 'a'
+        felboejt_verb.saetta_tempus(0)
+        if (self.kolla_med_stavningsjustering(felboejt_verb)):
+            felboejt_verb.saetta_tempus(-1)
+            return felboejt_verb.raettning
+        return None
+
+    def slut_ede(self, felboejt_verb):
+        felboejt_verb.raettning = felboejt_verb.raettning[:len(felboejt_verb.raettning) - 3] + "ade"
+        raettat_ord = self.grupp1(felboejt_verb)
+        return raettat_ord
+
+    def vokalfel(self, felboejt_verb):
+        provvokaler = []
+        uo = felboejt_verb.raettning  # sparar ursprungliga raettningsattributet i variabel uo (ursprungligt ord)
+        indx = 0
+        for bokstav in uo:
+            provvokaler = self.generera_provvokaler(bokstav)
+            if provvokaler == None:
+                indx += 1
+                continue
+            for vokal in provvokaler:
+                felboejt_verb.raettning = uo[:indx] + vokal + uo[indx + 1:]
+                #print("felboejt_verbs rättning med vokaljustering: " + felboejt_verb.raettning)
+                if self.kolla_med_stavningsjustering(felboejt_verb) != None:
+                    return felboejt_verb.raettning
+            indx += 1
+
+        felboejt_verb.raettning = uo
+        return None
+
+    def generera_provvokaler(self, bokstav):
+        provvokaler = []
+        if bokstav == 'a':
+            provvokaler = ['å', 'ä', 'ö']
+        elif bokstav == 'e':
+            provvokaler = ['u', 'ä', 'i']
+        elif bokstav == 'i':
+            provvokaler = ['y', 'e']
+        elif bokstav == 'o':
+            provvokaler = ['ö', 'å', 'u']
+        elif bokstav == 'u':
+            provvokaler = ['ö', 'o', 'y', 'e']
+        elif bokstav == 'y':
+            provvokaler = ['i', 'e']
+        elif bokstav == 'å':
+            provvokaler = ['a', 'o', 'ä', 'ö']
+        elif bokstav == 'ä':
+            provvokaler = ['a', 'e', 'å', 'u']
+        elif bokstav == 'ö':
+            provvokaler = ['a', 'o', 'u', 'å']
+        else:
+            provvokaler = None
+        return provvokaler
+
+    def dblkonsfel(self, felboejt_verb):
+        # kollar dubbelkonsonantfel
+        vokaler = ('a', 'e', 'i', 'o', 'u', 'y', 'å', 'ä', 'ö')
+
+        uo = felboejt_verb.raettning
+        indx = 1
+        while indx < len(uo):
+            if uo[indx] not in vokaler:
+                if uo[indx] == 'k':
+                    felboejt_verb.raettning = uo[:indx] + 'c' + uo[indx:]
+                else:
+                    felboejt_verb.raettning = uo[:indx] + uo[indx] + uo[indx:]
+                if self.kolla_med_stavningsjustering(felboejt_verb) != None:
+                    return felboejt_verb.raettning
+            if indx < len(uo):
+                indx += 1
+
+        indx = 0
+        while indx < len(uo)-1:
+            if uo[indx] not in vokaler:
+                if uo[indx] == uo[indx+1]:
+                    felboejt_verb.raettning = uo[:indx] + uo[indx+1:]
+                    if self.kolla_med_stavningsjustering(felboejt_verb) != None:
+                        return felboejt_verb.raettning
+            if indx < len(uo):
+                indx += 1
+
+        felboejt_verb.raettning = uo
+        return None
+
+class Ord:
+    def __init__(self, felboejt_verb):
+        self.fbv = felboejt_verb
+        self.tempus = -1
+        self.passiv = False
+        self.raettning = None
+        self.kraanglighet = 0
+
+        if (self.fbv[len(self.fbv)-1:] == 's'):
+            self.raettning = self.fbv[:len(self.fbv) - 1]
+            self.passiv = True
+        else:
+            self.raettning = self.fbv
+            if (self.fbv[:3] == 'umg') or (self.fbv[:4] == 'triv'):
+                self.passiv = True
+            else:
+                self.passiv = False
+        
+        """
+        krånglighetsbarometer:
+        0 = det skrivna ordet finns i verblistan, bara att hämta ut
+        1 = endast ändelsen behövde korrigeras med hjälp av en enkelt identifierbar rot
+        2 = en vokal var fel
+        3 = 1 + 2
+        4 = en konsonant behövde dubbleras eller en dubblad konsonant behövde tas bort
+        5 = 4 + 1
+        """
+
+        # pratade -> Passiv False + raettning = fbv
+        # pratades -> Passiv True + raettning = fbv-s
+        # umgådde -> Passiv True + raettning = fbv
+        # umgåddes -> Passiv False + raettning = fbv-s
+        
+    def saetta_tempus(self, tempus):
+        # infinitiv: -1
+        # presens:0
+        # imperfekt: 1
+        # particip: 2
+        self.tempus = tempus
+
+if __name__ == "__main__":
+
+    vd = VerbDetektiv(verblista)
+
+    x = str(input('\nSkriv ett verb!\n\n'))
+
+    while x != '00':
+        o = Ord(x.lower())
+        print(vd.raetta(o))
+        x = str(input('\n\nSkriv ett verb till!\n(00 för att avsluta)\n'))
+
